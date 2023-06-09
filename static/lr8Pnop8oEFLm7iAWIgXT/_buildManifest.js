c7dca2a673914c48401fc8a14dd53cd2f7605e38
jpoll962 <42617536+jpoll962@users.noreply.github.com>
2023-06-09 00:33:11 +0000

    Updates

---
+++ static/lr8Pnop8oEFLm7iAWIgXT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/357-da4c6173b06d5c95.js","static/chunks/pages/index-b16048e7f5f56137.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
